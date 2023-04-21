423d71e5bbdac03ec90e27bff19fffefec5b9c06
seungjae93 <lsj72023892@gmail.com>
2023-04-21 15:59:07 +0900

    :tada: [feat] 모두의중개 프로젝트

---
+++ src/shared/Router.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Route></Route>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
