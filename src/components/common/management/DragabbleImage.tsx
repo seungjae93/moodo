e399da96772475a9957592f42dfde6c5c1e4fc2f
seungjae93 <lsj72023892@gmail.com>
2023-05-11 14:01:46 +0900

    :sparkles: [feat] DragabbleImage component 분리

---
+++ src/components/common/management/DragabbleImage.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Draggable } from "react-beautiful-dnd";
+import styled from "styled-components";
+
+interface DragabbleImageProps {
+  key: string;
+  preview: string;
+  index: number;
+}
+
+function DragabbleImage({ preview, index }: DragabbleImageProps) {
+  return (
+    <Draggable key={preview} draggableId={preview} index={index}>
+      {(provided) => (
+        <StAvatarPreviewBox
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          <StAvatarPreview key={preview} src={preview} alt="Image" />
+        </StAvatarPreviewBox>
+      )}
+    </Draggable>
+  );
+}
+
+export default React.memo(DragabbleImage);
+
+const StAvatarPreviewBox = styled.div`
+  display: flex;
+  position: relative;
+`;
+const StAvatarPreview = styled.img`
+  width: 100px;
+  height: 100px;
+  background-color: transparent;
+`;
