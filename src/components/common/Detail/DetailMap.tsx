0b4d3eb9dac22171a7d3a4b7453beb2c129022d9
seungjae93 <lsj72023892@gmail.com>
2023-05-14 01:46:15 +0900

    :sparkles: [feat] DetailPage 1차 구현

--- src/components/common/Detail/DetailMap.tsx
+++ src/components/common/Detail/DetailMap.tsx
@@ -2,10 +2,13 @@ import { StDetail } from "../../../libs/styles/StDetail";
 
 function DetailMap() {
   return (
-    <StDetail.Title>
-      상세 위치{" "}
-      <span style={{ fontSize: "18px" }}>서울특별시 강남구 서초동</span>
-    </StDetail.Title>
+    <>
+      <StDetail.Title>
+        상세 위치{" "}
+        <span style={{ fontSize: "18px" }}>서울특별시 강남구 서초동</span>
+      </StDetail.Title>
+      <StDetail.Map>지도자리</StDetail.Map>
+    </>
   );
 }
 
