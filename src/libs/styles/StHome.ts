75a2a6ca0b0dd78cf7fc2cbc440e19ec69d5d8de
seungjae93 <lsj72023892@gmail.com>
2023-06-17 15:20:36 +0900

    :lipstick: [feat] StHome 애니매이션 추가

---
+++ src/libs/styles/StHome.ts
@@ -0,0 +1,242 @@
+import styled from "styled-components";
+import flex from "./utilFlex";
+
+import Background from "../../assets/BackGround.svg";
+
+interface PositionProps {
+  position: number;
+}
+
+export const StHome = {
+  Wrapper: styled.div`
+    ${flex({ justify: "", direction: "column", gap: "50px" })}
+    padding-top: 40px;
+    height: 100vh;
+    width: 1580px;
+    min-width: 1080px;
+    margin: auto;
+  `,
+  ContentWrapper: styled.div`
+    ${flex({ justify: "", direction: "column", gap: "150px" })}
+    width: 100%;
+  `,
+  Background: styled.div`
+    margin-top: 50px;
+    width: 100%;
+    height: 700px;
+    background: url(${Background});
+    background-repeat: no-repeat;
+    background-size: contain;
+  `,
+
+  CarouselImage: styled.img`
+    margin-left: 100px;
+    margin-top: 80px;
+    width: 650px;
+    height: 500px;
+  `,
+  CarouselContent: styled.div`
+    ${flex({ gap: "10px" })}
+    padding: 170px 0px 0px 120px;
+    h2 {
+      font-size: 40px;
+      margin-bottom: 20px;
+      color: black;
+    }
+    p {
+      font-size: 20px;
+      margin-bottom: 10px;
+      color: gray;
+    }
+    span {
+      font-weight: bold;
+    }
+    .largeText {
+      font-weight: bold;
+      color: black;
+    }
+  `,
+  MainContentContainer: styled.div`
+    ${flex({})}
+    width: 100%;
+    height: 600px;
+  `,
+  NaverBlogImg1: styled.img<PositionProps>`
+    margin-left: 50px;
+    width: 650px;
+    height: 600px;
+    position: ${(props) => (props.position > 700 ? "static" : "relative")};
+    transition: transform 0.4s ease-in-out, opacity 0.4s ease-in-out;
+    transform: ${(props) =>
+      props.position > 700 ? "translateY(0)" : "translateY(-50px)"};
+    opacity: ${(props) => (props.position > 700 ? 1 : 0)};
+  `,
+  NaverContent1: styled.div<PositionProps>`
+    ${flex({ align: "", direction: "column" })}
+    width: 700px;
+    height: 600px;
+    padding-left: 100px;
+    h2 {
+      font-size: 38px;
+      font-weight: bold;
+      margin-bottom: 20px;
+    }
+    p {
+      font-size: 18px;
+      margin-bottom: 10px;
+      font-weight: 500;
+      color: gray;
+    }
+    span {
+      font-weight: bold;
+    }
+    position: ${(props) => (props.position > 700 ? "static" : "relative")};
+    transition: transform 0.4s ease-in-out, opacity 0.4s ease-in-out;
+    transform: ${(props) =>
+      props.position > 700 ? "translateY(0)" : "translateY(-50px)"};
+    opacity: ${(props) => (props.position > 700 ? 1 : 0)};
+  `,
+  NaverBlogImg2: styled.img<PositionProps>`
+    margin-left: 50px;
+    width: 650px;
+    height: 600px;
+    position: ${(props) => (props.position > 1500 ? "static" : "relative")};
+    transition: transform 0.4s ease-in-out, opacity 0.4s ease-in-out;
+    transform: ${(props) =>
+      props.position > 1500 ? "translateY(0)" : "translateY(-50px)"};
+    opacity: ${(props) => (props.position > 1500 ? 1 : 0)};
+  `,
+  NaverContent2: styled.div<PositionProps>`
+    ${flex({ align: "", direction: "column" })}
+    width: 700px;
+    height: 600px;
+    padding-left: 100px;
+    h2 {
+      font-size: 38px;
+      font-weight: bold;
+      margin-bottom: 20px;
+    }
+    p {
+      font-size: 18px;
+      margin-bottom: 10px;
+      font-weight: 500;
+      color: gray;
+    }
+    span {
+      font-weight: bold;
+    }
+    position: ${(props) => (props.position > 1500 ? "static" : "relative")};
+    transition: transform 0.4s ease-in-out, opacity 0.4s ease-in-out;
+    transform: ${(props) =>
+      props.position > 1500 ? "translateY(0)" : "translateY(-50px)"};
+    opacity: ${(props) => (props.position > 1500 ? 1 : 0)};
+  `,
+  InstagramImg1: styled.img<PositionProps>`
+    margin-left: 50px;
+    width: 650px;
+    height: 600px;
+    position: ${(props) => (props.position > 2400 ? "static" : "relative")};
+    transition: transform 0.4s ease-in-out, opacity 0.4s ease-in-out;
+    transform: ${(props) =>
+      props.position > 2400 ? "translateY(0)" : "translateY(-50px)"};
+    opacity: ${(props) => (props.position > 2400 ? 1 : 0)};
+  `,
+  InstagramContent1: styled.div<PositionProps>`
+    ${flex({ align: "", direction: "column" })}
+    width: 700px;
+    height: 600px;
+    padding-left: 100px;
+    h2 {
+      font-size: 38px;
+      font-weight: bold;
+      margin-bottom: 20px;
+    }
+    p {
+      font-size: 18px;
+      margin-bottom: 10px;
+      font-weight: 500;
+      color: gray;
+    }
+    span {
+      font-weight: bold;
+    }
+    position: ${(props) => (props.position > 2400 ? "static" : "relative")};
+    transition: transform 0.4s ease-in-out, opacity 0.4s ease-in-out;
+    transform: ${(props) =>
+      props.position > 2400 ? "translateY(0)" : "translateY(-50px)"};
+    opacity: ${(props) => (props.position > 2400 ? 1 : 0)};
+  `,
+  InstagramImg2: styled.img<PositionProps>`
+    margin-left: 50px;
+    width: 650px;
+    height: 600px;
+    position: ${(props) => (props.position > 3000 ? "static" : "relative")};
+    transition: transform 0.4s ease-in-out, opacity 0.4s ease-in-out;
+    transform: ${(props) =>
+      props.position > 3000 ? "translateY(0)" : "translateY(-50px)"};
+    opacity: ${(props) => (props.position > 3000 ? 1 : 0)};
+  `,
+  InstagramContent2: styled.div<PositionProps>`
+    ${flex({ align: "", direction: "column" })}
+    width: 700px;
+    height: 600px;
+    padding-left: 100px;
+    h2 {
+      font-size: 38px;
+      font-weight: bold;
+      margin-bottom: 20px;
+    }
+    p {
+      font-size: 18px;
+      margin-bottom: 10px;
+      font-weight: 500;
+      color: gray;
+    }
+    span {
+      font-weight: bold;
+    }
+    position: ${(props) => (props.position > 3000 ? "static" : "relative")};
+    transition: transform 0.4s ease-in-out, opacity 0.4s ease-in-out;
+    transform: ${(props) =>
+      props.position > 3000 ? "translateY(0)" : "translateY(-50px)"};
+    opacity: ${(props) => (props.position > 3000 ? 1 : 0)};
+  `,
+  YoutubeImg: styled.img<PositionProps>`
+    margin-left: 50px;
+    margin-bottom: 150px;
+    width: 650px;
+    height: 600px;
+    position: ${(props) => (props.position > 4200 ? "static" : "relative")};
+    transition: transform 0.4s ease-in-out, opacity 0.4s ease-in-out;
+    transform: ${(props) =>
+      props.position > 4200 ? "translateY(0)" : "translateY(-50px)"};
+    opacity: ${(props) => (props.position > 4200 ? 1 : 0)};
+  `,
+
+  YoutubeContent: styled.div<PositionProps>`
+    ${flex({ align: "", direction: "column" })}
+    width: 700px;
+    height: 600px;
+    padding-left: 100px;
+    padding-bottom: 150px;
+    h2 {
+      font-size: 38px;
+      font-weight: bold;
+      margin-bottom: 20px;
+    }
+    p {
+      font-size: 18px;
+      margin-bottom: 10px;
+      font-weight: 500;
+      color: gray;
+    }
+    span {
+      font-weight: bold;
+    }
+    position: ${(props) => (props.position > 4200 ? "static" : "relative")};
+    transition: transform 0.4s ease-in-out, opacity 0.4s ease-in-out;
+    transform: ${(props) =>
+      props.position > 4200 ? "translateY(0)" : "translateY(-50px)"};
+    opacity: ${(props) => (props.position > 4200 ? 1 : 0)};
+  `,
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,11 @@
-import React, { useEffect, useRef } from "react";
-import styled from "styled-components";
+import React, { useEffect, useState } from "react";
+import styled, { keyframes, css } from "styled-components";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import flex from "../libs/styles/utilFlex";
 import NaverBlog from "../assets/NaverBlog.svg";
 import Youtube from "../assets/Youtube.svg";
 import Instagram from "../assets/Instagram.svg";
-import Background from "../assets/BackGround.svg";
 import SocialLogo from "../assets/SocialLogo.svg";
 import MapImage from "../assets/MapImage.svg";
 import NaverBlogExample1 from "../assets/NaverBlogExample1.svg";
@@ -16,13 +14,26 @@ import InstagramExample1 from "../assets/InstagramExample1.svg";
 import InstagramExample2 from "../assets/InstagramExample2.svg";
 import YoutubeExample from "../assets/YoutubeExample.svg";
 import Footer from "../components/common/Footer/Footer";
+import { StHome } from "../libs/styles/StHome";
 
 function Home() {
+  const [position, setPosition] = useState<number>(0);
+  const scroll = () => {
+    setPosition(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", scroll);
+    return () => {
+      window.removeEventListener("scroll", scroll);
+    };
+  }, []);
+
   const settings = {
     dots: false,
     infinite: true,
     autoplay: true,
-    autoplaySpeed: 3500,
+    autoplaySpeed: 4000,
     slidesToShow: 2,
     slidesToScroll: 2,
   };
@@ -58,10 +69,21 @@ function Home() {
               </StHome.CarouselContent>
             </Slider>
           </StHome.Background>
-          <img src={NaverBlog} />
+
+          <img
+            src={NaverBlog}
+            style={{
+              position: position > 300 ? "static" : "relative",
+              transition:
+                "transform 0.4s ease-in-out ,opacity 0.4s ease-in-out",
+              transform: position > 300 ? "translateY(0)" : "translateY(-50px)",
+              opacity: position > 300 ? 1 : 0,
+            }}
+          />
+
           <StHome.MainContentContainer>
-            <StHome.MainImage src={NaverBlogExample1} />
-            <StHome.MainContent>
+            <StHome.NaverBlogImg1 src={NaverBlogExample1} position={position} />
+            <StHome.NaverContent1 position={position}>
               <h2>블로그를 200%</h2>
               <h2>활용해보세요!</h2>
               <p style={{ marginTop: "30px" }}>
@@ -72,11 +94,11 @@ function Home() {
                 블로그로 유입되는 고객님을 홈페이지로 유입해
               </p>
               <p>더 많은 매물을 보여주고, 지역의 전문성을 보여주세요!</p>
-            </StHome.MainContent>
+            </StHome.NaverContent1>
           </StHome.MainContentContainer>
           <StHome.MainContentContainer>
-            <StHome.MainImage src={NaverBlogExample2} />
-            <StHome.MainContent>
+            <StHome.NaverBlogImg2 src={NaverBlogExample2} position={position} />
+            <StHome.NaverContent2 position={position}>
               <h2>정성스럽게 작성한</h2>
               <h2>포스팅에 홈페이지 링크를!</h2>
               <p style={{ marginTop: "30px" }}>포스팅에 홈페이지 링크를 넣어</p>
@@ -87,12 +109,22 @@ function Home() {
               <p>
                 마케팅 효율 <span>UP!</span>
               </p>
-            </StHome.MainContent>
+            </StHome.NaverContent2>
           </StHome.MainContentContainer>
-          <img src={Instagram} />
+          <img
+            src={Instagram}
+            style={{
+              position: position > 2100 ? "static" : "relative",
+              transition:
+                "transform 0.4s ease-in-out ,opacity 0.4s ease-in-out",
+              transform:
+                position > 2100 ? "translateY(0)" : "translateY(-50px)",
+              opacity: position > 2100 ? 1 : 0,
+            }}
+          />
           <StHome.MainContentContainer>
-            <StHome.MainImage src={InstagramExample1} />
-            <StHome.MainContent>
+            <StHome.InstagramImg1 src={InstagramExample1} position={position} />
+            <StHome.InstagramContent1 position={position}>
               <h2>인스타그램 프로필에</h2>
               <h2>홈페이지 링크를 달아보세요!</h2>
               <p style={{ marginTop: "30px" }}>
@@ -102,11 +134,11 @@ function Home() {
               <p style={{ marginTop: "30px" }}>
                 인스타그램의 마케팅 효율이 올라요!
               </p>
-            </StHome.MainContent>
+            </StHome.InstagramContent1>
           </StHome.MainContentContainer>
           <StHome.MainContentContainer>
-            <StHome.MainImage src={InstagramExample2} />
-            <StHome.MainContent>
+            <StHome.InstagramImg2 src={InstagramExample2} position={position} />
+            <StHome.InstagramContent2 position={position}>
               {" "}
               <h2>인스타그램의</h2>
               <h2>매물 게시글에 링크를 달아보세요!</h2>
@@ -117,12 +149,26 @@ function Home() {
               <p style={{ marginTop: "30px" }}>
                 인스타그램의 마케팅 효율이 올라요!
               </p>
-            </StHome.MainContent>
+            </StHome.InstagramContent2>
           </StHome.MainContentContainer>
-          <img src={Youtube} />
+          <img
+            src={Youtube}
+            style={{
+              position: position > 3900 ? "static" : "relative",
+              transition:
+                "transform 0.4s ease-in-out ,opacity 0.4s ease-in-out",
+              transform:
+                position > 3900 ? "translateY(0)" : "translateY(-50px)",
+              opacity: position > 3900 ? 1 : 0,
+            }}
+          />
           <StHome.MainContentContainer>
-            <StHome.MainImage className="small-content" src={YoutubeExample} />
-            <StHome.MainContent>
+            <StHome.YoutubeImg
+              className="small-content"
+              src={YoutubeExample}
+              position={position}
+            />
+            <StHome.YoutubeContent position={position}>
               {" "}
               <h2>유튜브 매물 영상에</h2>
               <h2>홈페이지 링크를 달아보세요</h2>
@@ -130,7 +176,7 @@ function Home() {
                 요즘 마케팅 대세는 유튜브입니다.
               </p>
               <p>유튜브로 매물 영상을 보는 고객님들을 홈페이지로 유도하세요!</p>
-            </StHome.MainContent>
+            </StHome.YoutubeContent>
           </StHome.MainContentContainer>
         </StHome.ContentWrapper>
         <Footer />
@@ -140,84 +186,3 @@ function Home() {
 }
 
 export default Home;
-
-const StHome = {
-  Wrapper: styled.div`
-    ${flex({ justify: "", direction: "column", gap: "50px" })}
-    padding-top: 40px;
-    height: 100vh;
-    width: 1580px;
-    min-width: 1080px;
-    margin: auto;
-  `,
-  ContentWrapper: styled.div`
-    ${flex({ justify: "", direction: "column", gap: "50px" })}
-    width: 100%;
-  `,
-  Background: styled.div`
-    margin-top: 50px;
-    width: 100%;
-    height: 700px;
-    background: url(${Background});
-    background-repeat: no-repeat;
-    background-size: contain;
-  `,
-
-  CarouselImage: styled.img`
-    margin-left: 100px;
-    margin-top: 80px;
-    width: 650px;
-    height: 500px;
-  `,
-  CarouselContent: styled.div`
-    ${flex({ gap: "10px" })}
-    padding: 170px 0px 0px 120px;
-    h2 {
-      font-size: 40px;
-      margin-bottom: 20px;
-      color: black;
-    }
-    p {
-      font-size: 20px;
-      margin-bottom: 10px;
-      color: gray;
-    }
-    span {
-      font-weight: bold;
-    }
-    .largeText {
-      font-weight: bold;
-      color: black;
-    }
-  `,
-  MainContentContainer: styled.div`
-    ${flex({})}
-    width: 100%;
-    height: 600px;
-  `,
-  MainImage: styled.img`
-    margin-left: 50px;
-    width: 650px;
-    height: 600px;
-  `,
-  MainContent: styled.div`
-    ${flex({ align: "", direction: "column" })}
-    width: 700px;
-    height: 600px;
-    padding-left: 100px;
-    h2 {
-      font-size: 38px;
-      font-weight: bold;
-      margin-bottom: 20px;
-    }
-    p {
-      font-size: 18px;
-      margin-bottom: 10px;
-      font-weight: 500;
-      color: gray;
-    }
-    span {
-      font-weight: bold;
-    }
-  `,
-};
